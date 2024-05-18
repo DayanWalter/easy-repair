99d04ee9c43cf9bb23a52c1883b1e41ea40d3c7e
Dayan Walter <dayanwalter@gmail.com>
2024-05-18 13:35:12 +0200

    Added orders and example dashboard

--- app/orders/layout.tsx
+++ app/orders/layout.tsx
@@ -72,11 +72,17 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 
-export default function Main() {
+import React from "react";
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
-      <aside className="fixed inset-y-0 left-0 z-10  flex w-14 flex-col border-r bg-background">
-        <nav className="flex flex-col items-center gap-4 px-2 py-5">
+      <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+        <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
           <Link
             href="/"
             className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
@@ -89,7 +95,7 @@ export default function Main() {
             <TooltipTrigger asChild>
               <Link
                 href="dashboard"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                className="flex h-9 w-9 items-center justify-center rounded-lg  text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
               >
                 <Home className="h-5 w-5" />
                 <span className="sr-only">Dashboard</span>
@@ -102,7 +108,7 @@ export default function Main() {
             <TooltipTrigger asChild>
               <Link
                 href="orders"
-                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent  text-accent-foreground  transition-colors hover:text-foreground md:h-8 md:w-8"
               >
                 <ShoppingCart className="h-5 w-5" />
                 <span className="sr-only">Orders</span>
@@ -151,7 +157,7 @@ export default function Main() {
           </Tooltip>
         </nav>
         {/* Settings */}
-        <nav className="mt-auto flex flex-col items-center gap-4 px-2 py-5">
+        <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
           <Tooltip>
             <TooltipTrigger asChild>
               <Link
@@ -166,6 +172,7 @@ export default function Main() {
           </Tooltip>
         </nav>
       </aside>
+      {children}
     </div>
   );
 }
