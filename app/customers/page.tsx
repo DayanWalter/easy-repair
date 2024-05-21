c2be939b0c5195a3959c5c7779bf4aec1ef1366c
Dayan Walter <dayanwalter@gmail.com>
2024-05-21 08:59:27 +0200

    Reduced col-span from cards

--- app/customers/page.tsx
+++ app/customers/page.tsx
@@ -230,7 +230,8 @@ export default function Customers() {
       <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
         <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
           <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
-            <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
+            {/* Your customers */}
+            <Card className="" x-chunk="dashboard-05-chunk-0">
               <CardHeader className="pb-3">
                 <CardTitle>Your Customers</CardTitle>
                 <CardDescription className="max-w-lg text-balance leading-relaxed">
@@ -272,6 +273,7 @@ export default function Customers() {
               </Button>
             </div>
           </div>
+          {/* Table */}
           <Card x-chunk="dashboard-05-chunk-3">
             <CardHeader className="px-7">
               <CardTitle>Customers</CardTitle>
--- app/products/page.tsx
+++ app/products/page.tsx
@@ -193,7 +193,7 @@ export default function Products() {
       <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
         <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
           <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
-            <Card className="sm:col-span-2" x-chunk="dashboard-05-chunk-0">
+            <Card className="" x-chunk="dashboard-05-chunk-0">
               <CardHeader className="pb-3">
                 <CardTitle>Your Products</CardTitle>
                 <CardDescription className="max-w-lg text-balance leading-relaxed">
