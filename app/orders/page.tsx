61c55b8ede11dba8ca3bf97b924a2f881ab9dc91
Dayan Walter <dayanwalter@gmail.com>
2024-05-22 10:37:56 +0200

    Changed size cards

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -192,7 +192,7 @@ export default function Orders() {
       </header>
       <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
         <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
-          <div className="grid gap-4 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-4">
+          <div className="grid xl:grid-cols-4">
             {/* Your Orders */}
             <Card className="" x-chunk="dashboard-05-chunk-0">
               <CardHeader className="pb-3">
