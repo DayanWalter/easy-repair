92f92618af3d32c7c04ebd15d29b0bf5b85f420f
Dayan Walter <dayanwalter@gmail.com>
2024-10-12 05:41:48 -0600

    fix: remove analytics tab from navbar

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,69 +1,69 @@
 import Link from "next/link";
 import {
-  Home,
-  LineChart,
-  Package,
-  Settings,
-  ShoppingCart,
-  Users2,
-  Wrench,
+	Home,
+	LineChart,
+	Package,
+	Settings,
+	ShoppingCart,
+	Users2,
+	Wrench,
 } from "lucide-react";
 
 import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger,
 } from "@/components/ui/tooltip";
 
 const navItems = [
-  { href: "/dashboard", icon: Home, label: "Dashboard" },
-  { href: "/orders", icon: ShoppingCart, label: "Orders" },
-  { href: "/products", icon: Package, label: "Products" },
-  { href: "/customers", icon: Users2, label: "Customers" },
-  { href: "/analytics", icon: LineChart, label: "Analytics" },
+	{ href: "/dashboard", icon: Home, label: "Dashboard" },
+	{ href: "/orders", icon: ShoppingCart, label: "Orders" },
+	{ href: "/products", icon: Package, label: "Products" },
+	{ href: "/customers", icon: Users2, label: "Customers" },
+	// { href: "/analytics", icon: LineChart, label: "Analytics" },
 ];
 
 const NavItem = ({
-  href,
-  icon: Icon,
-  label,
+	href,
+	icon: Icon,
+	label,
 }: {
-  href: string;
-  icon: React.ElementType;
-  label: string;
+	href: string;
+	icon: React.ElementType;
+	label: string;
 }) => (
-  <Tooltip>
-    <TooltipTrigger asChild>
-      <Link
-        href={href}
-        className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-      >
-        <Icon className="h-5 w-5" />
-        <span className="sr-only">{label}</span>
-      </Link>
-    </TooltipTrigger>
-    <TooltipContent side="right">{label}</TooltipContent>
-  </Tooltip>
+	<Tooltip>
+		<TooltipTrigger asChild>
+			<Link
+				href={href}
+				className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+			>
+				<Icon className="h-5 w-5" />
+				<span className="sr-only">{label}</span>
+			</Link>
+		</TooltipTrigger>
+		<TooltipContent side="right">{label}</TooltipContent>
+	</Tooltip>
 );
 
 export default function NavBar() {
-  return (
-    <aside className="fixed inset-y-0 left-0 z-10 flex w-14 flex-col border-r bg-background">
-      <nav className="flex flex-col items-center gap-4 px-2 py-5">
-        <Link
-          href="/"
-          className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
-        >
-          <Wrench className="h-4 w-4 transition-all group-hover:scale-110" />
-          <span className="sr-only">Easy Repair Inc</span>
-        </Link>
-        {navItems.map((item) => (
-          <NavItem key={item.href} {...item} />
-        ))}
-      </nav>
-      <nav className="mt-auto flex flex-col items-center gap-4 px-2 py-5">
-        <NavItem href="settings" icon={Settings} label="Settings" />
-      </nav>
-    </aside>
-  );
+	return (
+		<aside className="fixed inset-y-0 left-0 z-10 flex w-14 flex-col border-r bg-background">
+			<nav className="flex flex-col items-center gap-4 px-2 py-5">
+				<Link
+					href="/"
+					className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+				>
+					<Wrench className="h-4 w-4 transition-all group-hover:scale-110" />
+					<span className="sr-only">Easy Repair Inc</span>
+				</Link>
+				{navItems.map((item) => (
+					<NavItem key={item.href} {...item} />
+				))}
+			</nav>
+			<nav className="mt-auto flex flex-col items-center gap-4 px-2 py-5">
+				<NavItem href="settings" icon={Settings} label="Settings" />
+			</nav>
+		</aside>
+	);
 }
