2e3000f05fadd859718e8c8f6667882707f22b9c
Dayan Walter <dayanwalter@gmail.com>
2024-12-07 14:09:20 -0600

    feat: add basic dashboard

--- app/(dashboard-group)/orders/page.tsx
+++ app/(dashboard-group)/orders/page.tsx
@@ -35,8 +35,7 @@ export default async function Orders() {
 			<main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8">
 				<div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
 					<div className="grid xl:grid-cols-4">
-						{/* Your Orders */}
-						<Card className="" x-chunk="dashboard-05-chunk-0">
+						<Card>
 							<CardHeader className="pb-3">
 								<CardTitle>Ihre Bestellungen</CardTitle>
 								<CardDescription className="max-w-lg text-balance leading-relaxed">
