2fc495267d52a15ee259a96e5a14337bc9790645
Dayan Walter <dayanwalter@gmail.com>
2025-01-05 05:18:06 -0600

    fix: change favicon version

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,32 +7,35 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { Toaster } from "@/components/ui/toaster";
 
 const fontSans = FontSans({
-	subsets: ["latin"],
-	variable: "--font-sans",
+  subsets: ["latin"],
+  variable: "--font-sans",
 });
 export const metadata: Metadata = {
-	title: "Easy Repair",
-	description: "Let's repair stuff!",
+  title: "Easy Repair",
+  description: "Let's repair stuff!",
+  icons: {
+    icon: "/favicon.ico?v=2", // Version Query Parameter
+  },
 };
 
 export default async function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body
-				className={cn(
-					"min-h-screen bg-background font-sans antialiased",
-					fontSans.variable,
-				)}
-			>
-				<TooltipProvider>
-					{children}
-					<Toaster />
-				</TooltipProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en">
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <TooltipProvider>
+          {children}
+          <Toaster />
+        </TooltipProvider>
+      </body>
+    </html>
+  );
 }
